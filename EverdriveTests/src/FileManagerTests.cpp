d8ef0879568af36034cbfab8f54557a1b3010003
StevePro7 <steveprozune@hotmail.com>
2017-09-18 12:48:28 +0100

    Add UtilManager

--- EverdriveTests/src/FileManagerTests.cpp
+++ EverdriveTests/src/FileManagerTests.cpp
@@ -10,11 +10,11 @@ protected:
 	FileManager fileManager;
 };
 
-TEST_F(FileManagerTests, InsertCartridge)
-{
-	BYTE cartridgeMemory[0x10000];
-	memset(cartridgeMemory, 0, sizeof(cartridgeMemory));
-
-	fileManager.InsertCartridge("rom/MonacoGP.sms", cartridgeMemory);
-	ASSERT_EQ(0x31, cartridgeMemory[0]);
-}
+//TEST_F(FileManagerTests, InsertCartridge)
+//{
+//	BYTE cartridgeMemory[0x10000];
+//	memset(cartridgeMemory, 0, sizeof(cartridgeMemory));
+//
+//	fileManager.InsertCartridge("rom/MonacoGP.sms", cartridgeMemory);
+//	ASSERT_EQ(0x31, cartridgeMemory[0]);
+//}
