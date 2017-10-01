71e73cb6b6750f4264035e368d6658c4e25966e9
StevePro7 <steveprozune@hotmail.com>
2017-10-01 21:16:29 +0100

    Add Managers to Engine

--- EverdriveLibrary/src/Engine.h
+++ EverdriveLibrary/src/Engine.h
@@ -2,9 +2,12 @@
 #define _ENGINE_H_
 
 #include "Managers/ConfigManager.h"
+#include "Managers/CPUManager.h"
 #include "Managers/FileManager.h"
 #include "Managers/LogManager.h"
+#include "Managers/PSGManager.h"
 #include "Managers/UtilManager.h"
+#include "Managers/VDPManager.h"
 
 namespace Everdrive
 {
@@ -22,15 +25,21 @@ namespace Everdrive
 		void Quit();
 
 		ConfigManager& ConfigManager() { return configManager; }
+		CPUManager& CPUManager() { return cpuManager; }
 		FileManager& FileManager() { return fileManager; }
 		LogManager& LogManager() { return logManager; }
+		PSGManager& PSGManager() { return psgManager; }
 		UtilManager& UtilManager() { return utilManager; }
+		VDPManager& VDPManager() { return vdpManager; }
 
 	private:
 		Everdrive::ConfigManager configManager;
+		Everdrive::CPUManager cpuManager;
 		Everdrive::FileManager fileManager;
 		Everdrive::LogManager logManager;
+		Everdrive::PSGManager psgManager;
 		Everdrive::UtilManager utilManager;
+		Everdrive::VDPManager vdpManager;
 	};
 }
 
