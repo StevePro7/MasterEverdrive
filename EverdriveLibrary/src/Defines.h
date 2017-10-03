86025fc5075340c9100f8e342bfa958b17f12737
StevePro7 <steveprozune@hotmail.com>
2017-10-03 13:50:59 +0100

    Add new managers

--- EverdriveLibrary/src/Defines.h
+++ EverdriveLibrary/src/Defines.h
@@ -4,4 +4,11 @@
 #define ONE_MEGA_BYTE	0x100000
 #define SIXTY_FOUR_KB	0x10000
 
+namespace Everdrive
+{
+	static const int WINDOWWIDTH = 256;
+	static const int WINDOWHEIGHT = 192;
+	static const int SCREENSCALE = 1;
+}
+
 #endif//_DEFINES_H_
